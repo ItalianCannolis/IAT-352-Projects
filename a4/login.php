f461efceeb3debd88b048f338f5a51c821323e19
ItalianCannolis <nathan.cerone28@gmail.com>
2023-11-20 19:05:08 -0800

    Adjusted Login page to interact with DB and take some inputs

--- a4/login.php
+++ a4/login.php
@@ -10,14 +10,46 @@ if(isset($_SESSION['username'])){
     header("Location: showmodels.php");
 }
 
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     //Retrieve data from form
-    $username = $_POST['username'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
+    $pass_hash = password_hash($password, PASSWORD_DEFAULT);
+    $db = new mysqli('localhost','root','','classicmodels');
 
-    $sql = "SELECT username, hashed_password from "
+
+    $sql = "SELECT COUNT(*) FROM users";
+    $sql .= "WHERE email = ? AND encryptedPassword ='$pass_hash'";
     // not done am currently adding a new table in mysql to store the user information
+
+    $stmt = $db ->prepare($sql);
+    $stmt->bind_param('s',$name);
+    $stmt->execute();
+    $stmt->bind_result($count);
+
+    if($stmt -> fetch() && $count > 0){
+        $authenticated = $email;
+    } 
+    else{
+        $message = "Sorry email and password combination is not correct";
+    }
 }
 
 ?>
+
+<html>
+
+    <form method = "POST">
+        <label> Email </label>
+        <input type="textbox" name="email">
+        <label> Password </label>
+        <input type="textbox" name="password">
+        <input type = "submit" value = "Login">
+    </form>
+
+
+
+</html>
 
