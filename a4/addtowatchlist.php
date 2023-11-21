b9b177a4f6ef69ee4a6f92ce0c19067062fc7a8b
russellyuen1 <149857656+russellyuen1@users.noreply.github.com>
2023-11-20 23:49:20 -0800

    Update addtowatchlist.php

--- a4/addtowatchlist.php
+++ a4/addtowatchlist.php
@@ -4,7 +4,7 @@
 
     //check to see if the user's session has an email attached to it. If so, proceed to this page. If not, redirect to the login page.
     if (!isset($_SESSION['email'])) {
-        // Redirect to the login page if not logged in
+        // redirect to login pg
         $_SESSION['callback_url'] = 'addtowatchlist.php';
         header("Location: login.php");
         exit;
