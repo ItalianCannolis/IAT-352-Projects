f3942197246f898ddd24b799dff567b7e1f3db75
kjywong <kjywong.play@gmail.com>
2023-11-20 22:17:54 -0800

    comments for watch list

--- a4/addtowatchlist.php
+++ a4/addtowatchlist.php
@@ -2,5 +2,6 @@
     include 'header.php';
     include 'query_functions.php';
 
+    //check to see if the user's session has an email attached to it. If so, proceed to this page. If not, redirect to the login page.
 
 ?>
