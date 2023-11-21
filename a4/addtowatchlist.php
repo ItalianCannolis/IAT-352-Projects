df6340045f9207f5817426c226f1083f8e9988c5
russellyuen1 <149857656+russellyuen1@users.noreply.github.com>
2023-11-20 23:48:29 -0800

    Update addtowatchlist.php
    added a redirect for users who are not logged in

--- a4/addtowatchlist.php
+++ a4/addtowatchlist.php
@@ -3,5 +3,11 @@
     include 'query_functions.php';
 
     //check to see if the user's session has an email attached to it. If so, proceed to this page. If not, redirect to the login page.
+    if (!isset($_SESSION['email'])) {
+        // Redirect to the login page if not logged in
+        $_SESSION['callback_url'] = 'addtowatchlist.php';
+        header("Location: login.php");
+        exit;
+    }
 
 ?>
