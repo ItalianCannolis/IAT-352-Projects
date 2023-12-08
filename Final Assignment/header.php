5a41ec319afce91ee47f2a787760db52c73d51ff
kjywong <kjywong.play@gmail.com>
2023-12-08 00:09:16 -0800

    header

---
+++ Final Assignment/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>About</title>
+
+       
+       
+        <link href="./css/styles.css" rel="stylesheet">
+        
+    </head>
+
+        <header>
+            <h1 class="navtitle"><a href="index.html">Vinyl Visions</a></h1>
+            <nav class="navchild">
+                <ul class="nav_links"> 
+                    <li> <a href="login.php">Sign in</a> </li>
+                </ul>
+                
+            </nav>
+        </header>
