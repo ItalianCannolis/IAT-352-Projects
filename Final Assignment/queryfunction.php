d27ac673fa2bad84ca6f4c0336e2a7dd0572e34a
ItalianCannolis <nathan.cerone28@gmail.com>
2023-12-07 20:10:11 -0800

    Added Basic Search Query

--- Final Assignment/queryfunction.php
+++ Final Assignment/queryfunction.php
@@ -13,4 +13,19 @@
         }
         return $output;
     }
+
+    function find_cover_by_id($id) { //search for model by name 
+        $conn = db_connect();
+    
+        $sql = "SELECT * FROM cover_art ";
+        $sql .= "WHERE id ='" . db_escape($conn, $id) . "' ";
+        
+        // echo $sql;
+        $result = mysqli_query($conn, $sql);
+        confirm_result_set($result);
+        $model = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+        return $model; // returns an assoc. array
+        // db_disconnect($conn);
+      }
 ?>  
