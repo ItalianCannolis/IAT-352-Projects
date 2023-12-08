c748f26f6df0f5917877d25f5cf12a6442abaad5
ItalianCannolis <nathan.cerone28@gmail.com>
2023-12-07 23:15:34 -0800

    Showcase Generator Made but not well

--- Final Assignment/blob_reader.js
+++ Final Assignment/blob_reader.js
@@ -1,11 +1,30 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "cover");
-xhr.responseType = "blob";
-xhr.onload = response;
-xhr.send();
+$(document).ready(function() {
+// Hands-on 2
+  $('.generate_btn').on('click', function(event) {
+    // Make sure clicking this button will not redirect the page
+    event.preventDefault();
 
-function response(e) {
-    var urlCreator = window.URL || window.webkitURL;
-    var imageUrl = urlCreator.createObjectURL(this.response);
-    document.querySelector("#image").src = imageUrl;
- }
+    // Get the parent with data-prod_id
+    //var product_id = $(this).parents('li').data().prod_id;
+    var product_id = 5864;
+
+    $.ajax({
+      url: 'http://localhost/Iat-352-projects/Final%20Assignment/cover_art_collage.php',
+      data: {id: product_id},
+      success: function(res) {
+        console.log(res);
+        alert(res);
+    }
+
+    })
+    .done(function(res) {
+      alert(res);
+    })
+    .fail(function(res) {
+      console.log(res);
+      console.log("error");
+    })
+
+  })
+
+});
--- Final Assignment/cover_art_collage.php
+++ Final Assignment/cover_art_collage.php
@@ -3,13 +3,28 @@
 <head>|
 <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Your Website</title>
-    <script type="text/javascript" src="blob_reader.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/grid.css">
+    <!--<script type="text/javascript" src="blob_reader.js"></script> -->
+    <script type="text/javascript" src="cover_art_generator.js"></script>
 </head>
 <body>
+<div class = "collage">
+    <img width="390" height="390" id='image1' class = 'collageImg'/>
+    <img width="390" height="390" id='image2' class = 'collageImg'/>
+    <img width="390" height="390" id='image3' class = 'collageImg'/>
+    <img width="390" height="390" id='image4' class = 'collageImg'/>
+    <img width="390" height="390" id='image5' class = 'collageImg'/>
+    <img width="390" height="390" id='image6' class = 'collageImg'/>
+    <img width="390" height="390" id='image7' class = 'collageImg'/>
+    <img width="390" height="390" id='image8' class = 'collageImg'/>
+    <img width="390" height="390" id='image9' class = 'collageImg'/>
+    <img width="390" height="390" id='image10' class = 'collageImg'/>
+    <img width="390" height="390" id='image11' class = 'collageImg'/>
+    <img width="390" height="390" id='image12' class = 'collageImg'/>
+</div>
 
-<img id='image'/>
-
+<button type="button" class = "generate_btn">generate</button>
 </body>
 
 
