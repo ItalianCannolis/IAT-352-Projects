f92b1b45d65faea9623da54acdef6fd9109510e3
ItalianCannolis <nathan.cerone28@gmail.com>
2023-12-11 15:06:07 -0800

    Other files

--- Final Assignment/blob_reader.js
+++ Final Assignment/blob_reader.js
@@ -8,23 +8,38 @@ $(document).ready(function() {
     //var product_id = $(this).parents('li').data().prod_id;
     var product_id = 5864;
 
+    
     $.ajax({
-      url: 'http://localhost/Iat-352-projects/Final%20Assignment/cover_art_collage.php',
+      url: 'http://localhost/Iat-352-projects/Final%20Assignment/cover_art_getter.php',
       data: {id: product_id},
       success: function(res) {
-        console.log(res);
-        alert(res);
+        //console.log(res);
+        console.log("success");
+        //alert(res);
+
     }
 
     })
     .done(function(res) {
-      alert(res);
+      //alert(res);
+      console.log('done');
     })
     .fail(function(res) {
-      console.log(res);
+      //console.log(res);
       console.log("error");
     })
+    
+
+
+    $.get('http://localhost/Iat-352-projects/Final%20Assignment/cover_art_getter.php',blobToFile(image,"test.jpg"));
 
   })
 
 });
+
+function blobToFile(theBlob, fileName){
+  //A Blob() is almost a File() - it's just missing the two properties below which we will add
+  theBlob.lastModifiedDate = new Date();
+  theBlob.name = fileName;
+  return theBlob;
+}
