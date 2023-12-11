c977ee2fb2d293fa8020bae76c44416de02b5e8b
ItalianCannolis <nathan.cerone28@gmail.com>
2023-12-11 15:05:46 -0800

    Adjusted Query Functions to be able to pull blobs

--- Final Assignment/queryfunction.php
+++ Final Assignment/queryfunction.php
@@ -15,27 +15,10 @@
         return $output;
     }
 
-    function find_cover_by_id($id) { //search for model by name 
+    function find_cover_by_id($id) { //search for cover art by ID
         
         $conn = db_connect();
-        /*
-        $sql = "SELECT cover FROM cover_art ";
-        //$sql .= "WHERE id ='" . db_escape($conn, $id) . "' ";
-        
-        // echo $sql;
-        $result = mysqli_query($conn, $sql);
-        confirm_result_set($result);
 
-        //echo $result;
-        //return $result;
-        $model = mysqli_fetch_assoc($result);
-        mysqli_free_result($result);
-        return $model; // returns an assoc. array
-        // db_disconnect($conn);
-        */
-
-
-        //if (isset($_GET['image_id'])) {
             $sql = "SELECT cover, filetype FROM cover_art WHERE id=?";
             $statement = $conn->prepare($sql);
             $statement->bind_param("i", $id);
@@ -51,6 +34,5 @@
             $array[0] = $row["cover"];
             $array[1] = $row["filetype"];
             return $array;
-        //}
       }
 ?>  
