d6954d9e6d04c8759ea023d4f46a963fdff4220e
ItalianCannolis <nathan.cerone28@gmail.com>
2023-12-13 00:18:46 -0800

    Updated coverart getter to take advantage of sessions

--- Final Assignment/cover_art_getter.php
+++ Final Assignment/cover_art_getter.php
@@ -1,6 +1,7 @@
 <?php 
  include 'header.php';
  include 'queryfunction.php';
+ include 'cover_art_functions.php'
  ?>
 <!DOCTYPE html>
 <html lang='en'>
@@ -9,7 +10,7 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="css/grid.css">
-  <script type="text/javascript" src="blob_reader.js"></script> 
+    <!---<script type="text/javascript" src="blob_reader.js"></script>---> 
     <!--<script type="text/javascript" src="cover_art_generator.js"></script>-->
 </head>
 <body>
@@ -20,7 +21,12 @@
 
 
     <?php 
-        if(isset($_POST['collagename'])){
+    if(isset($_SESSION['user_id'])){
+       $collageNames = find_collage_owner_by_mem_id($_SESSION['user_id']);
+        generate_dropdown($collageNames);
+    }
+    
+    if(isset($_POST['collagename'])){
             $coverID = find_collage_id_by_name($_POST['collagename']);
             $ids = explode(",",$coverID[1]);
             shuffle($ids);
@@ -30,21 +36,22 @@
 
             $path = "img";
             for ($x = 0; $x < 12; $x++) {
-            //echo $ids[$x];
-                $image = find_cover_by_id(intval($ids[$x]));
+                //echo $ids[$x];
+                    $image = find_cover_by_id(intval($ids[$x]));
 
-                $fileExtension = $image[1];
+                    $fileExtension = $image[1];
 
-                $name = "collage".$x.$fileExtension;
-                // option 1
-                $file = fopen($path."/".$name,"w");
-                //echo "File name: ".$path."$name".    "\n";
-                fwrite($file, $image[0]);
+                    $name = "collage".$x.$fileExtension;
+                    // option 1
+                    $file = fopen($path."/".$name,"w");
+                    //echo "File name: ".$path."$name".    "\n";
+                    fwrite($file, $image[0]);
 
-                fclose($file);
-                echo '<img src="Img/'.$name . '" width="25%" height="25%" />';
+                    fclose($file);
+                    echo '<img src="Img/'.$name . '" width="25%" height="25%" />';
+            }
         }
-    }
+    
            //echo '<img src="Img/collage'.$x.'.jpg' . '" />';
 
     ?>
