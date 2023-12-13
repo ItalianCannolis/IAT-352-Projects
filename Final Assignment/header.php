cb0141dab9be28cdf1bd59f93618ff0b382946a2
ItalianCannolis <nathan.cerone28@gmail.com>
2023-12-13 15:30:15 -0800

    Header is now responsive to whether or not session is active

--- Final Assignment/header.php
+++ Final Assignment/header.php
@@ -12,18 +12,31 @@
         <nav class="navchild">
             <ul class="nav_links">
                 <?php
+                $url = $_SERVER['REQUEST_URI'];
+                $urlcheck = parse_url($url, PHP_URL_PATH);
+                $urlcheck = str_split($urlcheck);
+                $urlcheck = implode($urlcheck);
+                $urlcheck = explode('/',$urlcheck);
+                
+
                 if (isset($_SESSION['username'])) {
                     echo '<li><a href="logout.php">Logout</a></li>';
                     echo '<li><a href="profile.php">Profile</a></li>';
+                    echo "<li><a href='cover_art_getter.php'>Collage</a></li>";
+                    if(end($urlcheck) == "profile.php"){
+                        echo '<li><a href="edit_profile.php">Edit Profile</a></li>';
+                    }
                 } else {
                     // echo '<li><a href="login.php">Sign in</a></li>';
                     echo '<li><a href="register.php">Register</a></li>';
+                    echo "<li><button onclick='location.href = 'login.php';' id='myButton' class='submit-button'>Sign in</button></li>";
+
                 }
                 ?>
                 <li><a href="cover_art_search.php">Search Cover Arts</a></li>
                 <li><a href="cover_art_getter.php">Cover Art Catalogue</a></li>
-                <li><a href="cover_art_getter.php">Collage</a></li>
-                <li><button onclick="location.href = 'login.php';" id="myButton" class="submit-button">Sign in</button></li>
+                <!---<li><a href="cover_art_getter.php">Collage</a></li> -->
+                <li>
             </ul>
         </nav>
     </header>
