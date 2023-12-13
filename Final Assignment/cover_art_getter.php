6606a34454b3f82136b5cbc0285425561d60f643
ItalianCannolis <nathan.cerone28@gmail.com>
2023-12-13 13:40:20 -0800

    Cleaning up Getter Page, Allow them to make new cover_list from it

--- Final Assignment/cover_art_getter.php
+++ Final Assignment/cover_art_getter.php
@@ -23,39 +23,67 @@
 
     <?php 
     if(isset($_SESSION['mem_id'])){
-     
-       $collageNames = find_collage_owner_by_mem_id($_SESSION['mem_id']);
+        $path = "img";
+        $collageNames = find_collage_owner_by_mem_id($_SESSION['mem_id']);
 
-        generate_dropdown($collageNames);
-    }
+        if($collageNames != null){
+            if(isset($_POST['collagename'])){
+                    $coverID = find_collage_id_by_name($_POST['collagename']);
+                    $ids = explode(",",$coverID[1]);
+                    shuffle($ids);
 
-    if(isset($_POST['collagename'])){
-            $coverID = find_collage_id_by_name($_POST['collagename']);
+                    storeDispImages($ids, $path);
+                }
+            else if(!isset($_POST['collagename'])){
+                $collageNames = find_collage_owner_by_mem_id($_SESSION['mem_id']);
+                $coverID = find_collage_id_by_name($collageNames[0]);
+                $ids = explode(",",$coverID[1]);
+                shuffle($ids);
+
+                storeDispImages($ids, $path);
+            }
+            generate_dropdown($collageNames);
+        }
+        else{
+            echo "<h2> you don't have any collages yet or you don't have anything in them, better get on that!</h2>";
+        }
+
+        generate_new_collage_form();
+       
+        if(isset($_POST['newCollage'])){
+            create_new_collage_by_mem_id($_SESSION['mem_id'],$_POST['newCollage']);
+            //$coverID = find_collage_id_by_name($_POST['collagename']);
             $ids = explode(",",$coverID[1]);
             shuffle($ids);
 
-
-        
-
-            $path = "img";
-            for ($x = 0; $x < 12; $x++) {
-                //echo $ids[$x];
-                    $image = find_cover_by_id(intval($ids[$x]));
-
-                    $fileExtension = $image[1];
-
-                    $name = "collage".$x.$fileExtension;
-                    // option 1
-                    $file = fopen($path."/".$name,"w");
-                    //echo "File name: ".$path."$name".    "\n";
-                    fwrite($file, $image[0]);
-
-                    fclose($file);
-                    echo '<img src="Img/'.$name . '" width="25%" height="25%" />';
-            }
+            storeDispImages($ids, $path);
         }
+
+   
+        
+    }
     
            //echo '<img src="Img/collage'.$x.'.jpg' . '" />';
+
+
+    function storeDispImages($ids, $path){
+
+        for ($x = 0; $x < 12; $x++) {
+            //echo $ids[$x];
+                $image = find_cover_by_id(intval($ids[$x]));
+
+                $fileExtension = $image[1];
+
+                $name = "collage".$x.$fileExtension;
+                // option 1
+                $file = fopen($path."/".$name,"w");
+                //echo "File name: ".$path."$name".    "\n";
+                fwrite($file, $image[0]);
+
+                fclose($file);
+                echo '<img src="Img/'.$name . '" width="25%" height="25%" />';
+        }
+    }
 
     ?>
 
