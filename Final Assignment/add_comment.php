10a8eefb6ee69a90aa4d1b07505ac2b311a9a973
russellyuen1 <149857656+russellyuen1@users.noreply.github.com>
2023-12-13 21:12:07 -0800

    Update add_comment.php

--- Final Assignment/add_comment.php
+++ Final Assignment/add_comment.php
@@ -1,26 +1,22 @@
 <?php
-session_start(); // Start the session
+session_start();
 
-// Check if the user is logged in or not, and redirect if not logged in
 if (!isset($_SESSION['user_id'])) {
-    // Redirect the user to the login page or any other appropriate action
-    header("Location: login.php"); // Change 'login.php' to your actual login page
+    header("Location: login.php"); // Redirect to login if not logged in
     exit();
 }
 
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Assuming you have a database connection established ($connection)
+    // Retrieve form data
     $userId = $_SESSION['user_id'];
     $albumId = $_POST['albumId'];
     $commentText = $_POST['commentText'];
 
-    // Insert the comment into the database
+    // Insert comment into the database
     $insert_comment_query = "INSERT INTO comments (user_id, album_id, comment_text) VALUES ($userId, $albumId, '$commentText')";
     mysqli_query($connection, $insert_comment_query);
 
-    // Redirect back to the album details page after adding the comment
-    header("Location: album_details.php?id=$albumId"); // Change 'album_details.php' to your actual page
+    header("Location: album_details.php?id=$albumId"); // redirect to album details page
     exit();
 }
 ?>
